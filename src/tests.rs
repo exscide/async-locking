9a7f562c004ff3ff7257513fe97ab5d6e3a5e55d
exscide <exscide@gmail.com>
2024-04-16 18:18:13 +0200

    test os behavior

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,7 @@
+use libc::EBADF;
+
 use super::*;
-use std::{ io::{ BufRead, BufReader }, process::{ Child, ChildStdout, ExitStatus, Stdio } };
+use std::{ io::{ BufRead, BufReader, Error }, process::{ Child, ChildStdout, ExitStatus, Stdio }, sync::mpsc::channel, time::Duration };
 
 struct Process {
 	inner: Child,
@@ -64,7 +66,7 @@ async fn open_file(path: &str) -> std::fs::File {
 		.unwrap()
 }
 
-#[cfg_attr(feature = "tokio", tokio::test)]
+#[cfg_attr(feature = "tokio", tokio::test(flavor = "multi_thread", worker_threads = 4))]
 #[cfg_attr(feature = "async-std", async_std::test)]
 #[cfg_attr(feature = "blocking", async_std::test)]
 async fn test_lock_interprocess() {
@@ -112,6 +114,9 @@ async fn test_lock_interprocess() {
 	}
 
 	lock.unlock().await.unwrap();
+
+	#[cfg(feature = "tokio")]
+	os_test().await;
 }
 
 
@@ -136,4 +141,67 @@ async fn test_lock_current_process() {
 	assert!(file.try_lock_exclusive().unwrap().is_none());
 
 	lock.unlock().unwrap();
+
+	let lock = file.try_lock_shared()
+		.unwrap()
+		.unwrap();
+	assert!(file2.try_lock_shared().unwrap().is_some());
+	assert!(file2.try_lock_exclusive().unwrap().is_none());
+
+	lock.unlock().unwrap();
+}
+
+
+#[cfg(feature = "tokio")]
+async fn os_test() {
+	use std::os::unix::io::AsRawFd;
+
+	// lock the file
+	let mut blck = blocker();
+	blck.wait_for("ready").unwrap();
+
+	let mut f = std::fs::File::options()
+		.create(true)
+		.write(true)
+		.open("target/test.lock")
+		.unwrap();
+
+	assert!(f.try_lock_exclusive().unwrap().is_none());
+
+	let fd = f.as_raw_fd();
+
+	// it seems like the drop call blocks until the lock call is done,
+	// ensure this behavior across platforms
+
+	let (s, r) = channel();
+
+	let waiter = tokio::spawn(async move {
+		s.send(()).unwrap();
+		println!("waiting");
+		tokio::task::spawn_blocking(move || lock_exclusive(fd)).await.unwrap()
+	});
+
+	let dropper = tokio::spawn(async move {
+		r.recv().unwrap();
+		std::thread::sleep(Duration::from_secs(1));
+		println!("dropping file");
+		drop(f);
+		println!("dropped file");
+	});
+
+	tokio::time::timeout(Duration::from_secs(2), async { println!("waiting for dropper"); dropper.await })
+		.await
+		.expect_err("drop should wait for lock");
+
+	blck.kill().unwrap();
+	waiter.await
+		.unwrap()
+		.unwrap();
+
+	// trying to lock a dropped file should error
+	#[cfg(not(windows))]
+	let err = EBADF;
+	#[cfg(windows)]
+	let err = 0;
+	assert_eq!(lock_exclusive(fd).map_err(|e| e.raw_os_error()), Err(Some(err)));
 }
--- src/unix.rs
+++ src/unix.rs
@@ -1,24 +1,25 @@
-use rustix::fs::{ flock, FlockOperation };
-use std::{ io::ErrorKind, os::unix::io::AsFd };
+use std::{ io::{ Error, ErrorKind }, os::fd::AsRawFd };
+
+use libc::{c_int, LOCK_EX, LOCK_NB, LOCK_SH, LOCK_UN};
 
 
 /// Catchall trait for [File](std::fs::File) like types
-pub trait AsDescriptor: AsFd + Send + 'static {}
-impl<T: AsFd + Send + 'static> AsDescriptor for T {}
+pub trait AsDescriptor: AsRawFd + Send + 'static {}
+impl<T: AsRawFd + Send + 'static> AsDescriptor for T {}
 
 
-pub(crate) fn lock_shared<F: AsFd>(file: F) -> std::io::Result<F> {
-	lock_file(&file, FlockOperation::LockShared)?;
+pub(crate) fn lock_shared<F: AsRawFd>(file: F) -> std::io::Result<F> {
+	lock_file(&file, LOCK_SH)?;
 	Ok(file)
 }
 
-pub(crate) fn lock_exclusive<F: AsFd>(file: F) -> std::io::Result<F> {
-	lock_file(&file, FlockOperation::LockExclusive)?;
+pub(crate) fn lock_exclusive<F: AsRawFd>(file: F) -> std::io::Result<F> {
+	lock_file(&file, LOCK_EX)?;
 	Ok(file)
 }
 
-pub(crate) fn try_lock_shared<F: AsFd>(file: &F) -> std::io::Result<Option<()>> {
-	let res = lock_file(file, FlockOperation::NonBlockingLockShared);
+pub(crate) fn try_lock_shared<F: AsRawFd>(file: &F) -> std::io::Result<Option<()>> {
+	let res = lock_file(file, LOCK_SH | LOCK_NB);
 
 	if let Err(e) = &res {
 		if let ErrorKind::WouldBlock = e.kind() {
@@ -29,8 +30,8 @@ pub(crate) fn try_lock_shared<F: AsFd>(file: &F) -> std::io::Result<Option<()>> 
 	res.map(|_| Some(()))
 }
 
-pub(crate) fn try_lock_exclusive<F: AsFd>(file: &F) -> std::io::Result<Option<()>> {
-	let res = lock_file(file, FlockOperation::NonBlockingLockExclusive);
+pub(crate) fn try_lock_exclusive<F: AsRawFd>(file: &F) -> std::io::Result<Option<()>> {
+	let res = lock_file(file, LOCK_EX | LOCK_NB);
 
 	if let Err(e) = &res {
 		if let ErrorKind::WouldBlock = e.kind() {
@@ -41,14 +42,21 @@ pub(crate) fn try_lock_exclusive<F: AsFd>(file: &F) -> std::io::Result<Option<()
 	res.map(|_| Some(()))
 }
 
-pub(crate) fn unlock<F: AsFd>(file: F) -> std::io::Result<F> {
+pub(crate) fn unlock<F: AsRawFd>(file: F) -> std::io::Result<F> {
 	unlock_ref(&file).map(|_| file)
 }
 
-pub(crate) fn unlock_ref<F: AsFd>(file: &F) -> std::io::Result<()> {
-	lock_file(file, FlockOperation::Unlock)
+pub(crate) fn unlock_ref<F: AsRawFd>(file: &F) -> std::io::Result<()> {
+	lock_file(file, LOCK_UN)
 }
 
-fn lock_file<F: AsFd>(file: F, op: FlockOperation) -> std::io::Result<()> {
-	flock(file, op).map_err(|e| e.into())
+fn lock_file<F: AsRawFd>(file: &F, op: c_int) -> std::io::Result<()> {
+	let res = unsafe {
+		libc::flock(file.as_raw_fd(), op)
+	};
+
+	match res {
+		0 => Ok(()),
+		_ => Err(Error::last_os_error())
+	}
 }
